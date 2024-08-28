74042934affcd440e1bca07456c0f009763b811b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-28 12:15:53 -0400

    chore: Update host in postgres.js to use IP address instead of 'localhost'

--- libs/postgres.js
+++ libs/postgres.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 async function getConnection() {
     const client = new Client({
-        host: 'localhost',
+        host: '172.18.0.2',
         port: 5432,
         user: 'javier',
         password: '12345',
